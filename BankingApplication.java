9bcc4042fc1963c533a2ad0afbf7d2cc7f2fdf60
Stephen ombuya <129269603+STEPHEN07383@users.noreply.github.com>
2023-08-04 14:40:26 +0300

    Add files via upload

---
+++ BankingApplication.java
@@ -0,0 +1,12 @@
+package bankingApplication;
+
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		BankAccount obj = new BankAccount("VS DevCode", "VS001");
+		obj.showMenu();
+		
+
+	}
+
+}
