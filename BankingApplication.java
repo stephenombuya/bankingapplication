610c10cf3df98f1c64381f3b42ab8970dce9973d
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2024-12-17 23:36:42 +0300

    Update BankingApplication.java

--- BankingApplication.java
+++ BankingApplication.java
@@ -1,12 +1,18 @@
 package bankingApplication;
 
+import java.util.Scanner;
+
 public class BankingApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		BankAccount obj = new BankAccount("VS DevCode", "VS001");
-		obj.showMenu();
-		
+        System.out.print("Enter customer name: ");
+        String name = scanner.nextLine();
 
-	}
+        System.out.print("Enter customer ID: ");
+        String id = scanner.nextLine();
 
+        BankAccount account = new BankAccount(name, id);
+        account.showMenu();
+    }
 }
